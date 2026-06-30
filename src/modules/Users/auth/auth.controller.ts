0a3e860a2d4e506cd473cc334bb966d307d8af28
kauatatim <kaua.tatim.duarte@escola.pr.gov.br>
2026-06-30 00:31:21 -0300

    edit

--- src/modules/Users/auth/auth.controller.ts
+++ src/modules/Users/auth/auth.controller.ts
@@ -1,1 +1,14 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  // POST /auth/login
+  // Body: { "emailGoogle": "...", "senha": "..." }
+  @Post('login')
+  login(@Body() body: any) {
+    return this.authService.login(body.emailGoogle, body.senha);
+  }
+}
