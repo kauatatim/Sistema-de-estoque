0f87599c6f213d865918739fd925af7ebad62110
kauatatim <kaua.tatim.duarte@escola.pr.gov.br>
2026-06-30 00:40:35 -0300

    Add JwtStrategy for JWT authentication
    Implement JWT strategy for authentication using Passport.

---
+++ src/modules/Users/auth/jwt.strategy.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'minha_chave_super_secreta_2024', // depois trocamos por .env
+    });
+  }
+
+  async validate(payload: any) {
+    return {
+      id: payload.sub,
+      nome: payload.nome,
+      emailGoogle: payload.emailGoogle,
+      tipo_perfil: payload.tipo_perfil,
+    };
+  }
+}
